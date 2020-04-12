3d18c37dd71eda81ebbbbea4023b44b564e70f7f
linqining <linqining@outlook.com>
2020-04-12 01:13:46 -0700

    change contact

--- src/components/layout/IcpInfo.vue
+++ src/components/layout/IcpInfo.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="icp-info">
+        <div>联系电话：023-88213504</div>
         <span><a href="http://www.beian.miit.gov.cn/" target="_blank">渝ICP备20001918号</a></span>
         <div>重庆瑞之铭商贸有限公司 版权所有</div>
         <div>公司地址：重庆市江北区华新村96号5幢16-4</div>
